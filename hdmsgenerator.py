985d2926368d722e27c18c66b6f8b104922c86e2
Yoshihiro Kita <kita.yoshihiro@gmail.com>
2019-07-29 16:01:33 +0900

    a

--- hdmsgenerator.py
+++ hdmsgenerator.py
@@ -13,7 +13,7 @@ class HDMSGenerator(object):
         self.train_X, self.train_y = zip(*traindata)
         self.offsets = [0, len(train_y[train_y == 0]), len(train_y)]
 
-    def generate(self):
+    def generate(self, simple=False):
         """
         batchサイズ分のデータを作ってyieldし続けるgenerator
         """
@@ -34,7 +34,10 @@ class HDMSGenerator(object):
                 else:
                     ret_X.append(self.train_X[XIDs[0]])
                 ret_X2.append(self.train_X[XIDs[1]])
-            yield np.array(ret_X)[:,:,:,np.newaxis], [to_categorical(ret_y, num_classes=2), np.array(ret_X2).reshape(self.batch_size, -1)]
+            if simple:
+                yield np.array(ret_X)[:,:,:,np.newaxis], to_categorical(ret_y, num_classes=2)
+            else:
+                yield np.array(ret_X)[:,:,:,np.newaxis], [to_categorical(ret_y, num_classes=2), np.array(ret_X2).reshape(self.batch_size, -1)]
 
 if __name__ == '__main__':
     with open('../20190719_bin_1_ndarray.pickle', mode='rb') as f:
