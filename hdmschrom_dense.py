20d2b08ec1f10f32df73cb95e3295ac8cc91d917
Yoshihiro Kita <kita.yoshihiro@gmail.com>
2019-07-29 15:56:09 +0900

    a

--- hdmschrom_dense.py
+++ hdmschrom_dense.py
@@ -5,12 +5,10 @@ if K.backend() == 'cntk':
     import cntk as C
 import keras
 from keras.layers import Activation
-from keras.layers import AtrousConvolution2D
 from keras.layers import Conv2D
 from keras.layers import Dense
 from keras.layers import Flatten
 from keras.layers import Concatenate
-from keras.layers import GlobalAveragePooling2D
 from keras.layers import Input
 from keras.layers import MaxPooling2D
 from keras.layers import UpSampling2D
@@ -20,13 +18,6 @@ from keras.layers import Reshape
 from keras.layers import ZeroPadding2D
 from keras.models import Model
 
-from mschromnet_layers import PriorBox
-from mschromnet_layers import Normalize
-from mschromnet_layers import MagnifyAndClip
-from mschromnet_layers import LogTransform
-
-from mschromnet_utils import BBoxUtility
-from gdrivegenerator import GdriveGenerator
 import pickle
 
 def HDMSDenseNet(input_shape, num_classes=2):
@@ -38,10 +29,17 @@ def HDMSDenseNet(input_shape, num_classes=2):
     net['flatten_1'] = Flatten(name='flatten_1')(net['input_1'])  
 
     # Class Prediction
-    net['dense_1'] = Dense(64, name='dense_1', activation='relu')(net['flatten_1'])
-    net['dense_2'] = Dense(64, name='dense_2', activation='relu')(net['dense_1'])
-    net['dense_3'] = Dense(64, name='dense_3', activation='relu')(net['dense_2'])
-    net['class_prediction'] = Dense(2, name ='class_prediction', activation='softmax')(net['dense_3'])
+    net['dense_1'] = Dense(64, name='dense_1')(net['flatten_1'])
+    net['batchnorm_1'] = BatchNormalization(name='batchnorm_1')(net['dense_1'])
+    net['activation_1'] = Activation(activation='relu', name='activation_1')(net['batchnorm_1'])
+    net['dense_2'] = Dense(64, name='dense_2')(net['activation_1'])
+    net['batchnorm_2'] = BatchNormalization(name='batchnorm_2')(net['dense_2'])
+    net['activation_2'] = Activation(activation='relu', name='activation_2')(net['batchnorm_2'])
+    net['dense_3'] = Dense(64, name='dense_3')(net['activation_2'])
+    net['batchnorm_3'] = BatchNormalization(name='batchnorm_3')(net['dense_3'])
+    net['activation_3'] = Activation(activation='relu', name='activation_3')(net['batchnorm_3'])
+
+    net['class_prediction'] = Dense(2, name ='class_prediction', activation='softmax')(net['activation_3'])
 
     model = Model(net['input_1'], net['class_prediction'])
     return model
