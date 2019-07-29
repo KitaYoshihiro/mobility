19172cd3a487bb4260d9a65ec9bed4aac01e16f4
Yoshihiro Kita <kita.yoshihiro@gmail.com>
2019-07-29 14:44:04 +0900

    a

--- sgan_hdms.py
+++ sgan_hdms.py
@@ -1,14 +1,17 @@
 from __future__ import print_function, division
 
-from keras.datasets import mnist
-from keras.layers import Input, Dense, Reshape, Flatten, Dropout, multiply, GaussianNoise, Concatenate
-from keras.layers import BatchNormalization, Activation, Embedding, ZeroPadding2D, MaxPooling2D
-from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.convolutional import UpSampling2D, Conv2D
-from keras.models import Sequential, Model
-from keras.optimizers import Adam
-from keras import losses
-from keras.utils import to_categorical
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.layers import Input, Dense, Reshape, Flatten, Dropout, multiply, GaussianNoise, Concatenate
+from tensorflow.keras.layers import BatchNormalization, Activation, Embedding, ZeroPadding2D, MaxPooling2D
+
+#from keras.layers.advanced_activations import LeakyReLU
+#from keras.layers.convolutional import UpSampling2D, Conv2D
+from tensorflow.keras.layers import UpSampling2D, Conv2D, LeakyReLU
+
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import losses
+from tensorflow.keras.utils import to_categorical
 import keras.backend as K
 
 import pickle
